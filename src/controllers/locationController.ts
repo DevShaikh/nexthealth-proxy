210e32549545b964d3eb1b73ee4e1fd02517498a
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-15 11:45:35 +0500

    cleanup

--- src/controllers/locationController.ts
+++ src/controllers/locationController.ts
@@ -3,15 +3,13 @@ import { RequestHandler } from "express";
 import ApiError from "../utils/ApiError";
 import { sendSuccessResponse } from "../utils/responseHandler";
 
+import { GetLocationsInput } from "validations/schemas/locationSchema";
+
 import * as locationService from "../services/locationService";
 
 export const getLocations: RequestHandler = async (req, res, next) => {
   try {
-    const { subdomain } = req.query as { subdomain?: string };
-
-    if (!subdomain) {
-      throw new ApiError(401, "Subdomain is required");
-    }
+    const { subdomain } = req.query as GetLocationsInput;
 
     const result = await locationService.getLocations(subdomain);
 
