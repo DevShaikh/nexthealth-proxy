51ed6605b232a16e989a046874f7cff3350e6d09
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-15 09:40:37 +0500

    Routes: Get Locations

---
+++ src/routes/locationRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import * as locationController from "../controllers/locationController";
+
+const router = Router();
+
+router.get("/", locationController.getLocations);
+
+export default router;
