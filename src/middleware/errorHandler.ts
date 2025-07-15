31ac53f1f8ad39e17763322b53c850ec2d47a3ee
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-15 09:38:52 +0500

    Essential Middlewares

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,45 @@
+/**
+ * @file src/middleware/errorHandler.ts
+ * @description Global error handling middleware for Express.
+ */
+
+import { Request, Response, NextFunction } from "express";
+
+import env from "../config/env";
+
+import ApiError from "../utils/ApiError";
+
+// Error handling middleware
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let error = err;
+
+  // If the error is not an operational error, convert it to one
+  if (!(error instanceof ApiError)) {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || "Something went wrong";
+    error = new ApiError(statusCode, message, false, error.stack);
+  }
+
+  const { statusCode, message, isOperational } = error;
+
+  const response = {
+    success: false,
+    code: statusCode,
+    message: message,
+    errors: isOperational ? error.errors : undefined, // Only include errors if it's an operational error
+    ...(env.NODE_ENV === "development" && { stack: error.stack }), // Only show stack in dev
+  };
+
+  if (env.NODE_ENV === "development") {
+    console.error("Caught by error handler:", error);
+  }
+
+  res.status(statusCode).send(response);
+};
+
+export default errorHandler;
---
+++ src/middleware/logger.ts
@@ -0,0 +1,18 @@
+/**
+ * @file src/middleware/logger.ts
+ * @description Middleware for logging incoming HTTP requests.
+ */
+
+import { Request, Response, NextFunction } from "express";
+import env from "../config/env";
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  if (env.NODE_ENV === "development") {
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`
+    );
+  }
+  next();
+};
+
+export default logger;
