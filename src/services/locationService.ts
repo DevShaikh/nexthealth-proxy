7a188a1f5d0474cd57b8642cd24b16f5f70b6d27
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-15 09:40:19 +0500

    Location Service: Get Locations

---
+++ src/services/locationService.ts
@@ -0,0 +1,40 @@
+import axios, { AxiosResponse } from "axios";
+
+import ApiError from "../utils/ApiError";
+
+import { Location, NextHealthAPIResponse } from "../types";
+
+import env from "../config/env";
+
+export const getLocations = async (subdomain: string) => {
+  try {
+    const { data }: AxiosResponse<NextHealthAPIResponse<Location[]>> =
+      await axios.get(
+        `${env.NEXT_HEALTH_API_URL}/locations?subdomain=${subdomain}`,
+        {
+          headers: {
+            Authorization: process.env.NEXT_HEALTH_API_TOKEN,
+          },
+        }
+      );
+
+    const { data: locations } = data;
+
+    console.log(
+      `[LOCATIONS] Fetched: ${locations?.length} locations`,
+      locations
+    );
+
+    return locations || [];
+  } catch (error) {
+    console.error("[LOCATIONS] Error:", error);
+    throw error instanceof ApiError
+      ? error
+      : new ApiError(
+          500,
+          `Failed to fetch locations: ${
+            error instanceof Error ? error.message : "Unknown error"
+          }`
+        );
+  }
+};
