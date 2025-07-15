7cd0e33fe303a9f6450a435ba468dc75499f02fd
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-15 12:39:31 +0500

    Exernal API Interceptor

---
+++ src/utils/externalApi.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+import env from "../config/env";
+
+const externalApi = axios.create({
+  baseURL: env.NEXT_HEALTH_API_URL,
+});
+
+externalApi.interceptors.request.use(
+  (config) => {
+    if (!!env.NEXT_HEALTH_API_TOKEN) {
+      config.headers.Authorization = `${env.NEXT_HEALTH_API_TOKEN}`;
+    } else {
+      console.warn(
+        "[Axios Interceptor] NEXT_HEALTH_API_TOKEN is not set or is using default value."
+      );
+    }
+
+    console.error("[Axios Interceptor] Request:", config.url);
+
+    return config;
+  },
+  (error) => {
+    // Handle request errors (e.g., network issues before sending)
+    console.error("[Axios Interceptor] Request error:", error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default externalApi;
