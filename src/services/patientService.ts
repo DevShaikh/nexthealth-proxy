4dab1b6351591599b2bb9f3f42a88c08d35f9c72
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-22 12:16:10 +0500

    Patient Service: Create Patient

--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -5,9 +5,13 @@ import ApiError from "../utils/ApiError";
 
 import { NextHealthAPIResponse, Patient } from "../types";
 
+import { CreatePatientBodyInput } from "../validations/schemas/patientSchema";
+
 export const getPatients = async (params: string) => {
   try {
-    const { data }: AxiosResponse<NextHealthAPIResponse<Patient[]>> =
+    const {
+      data,
+    }: AxiosResponse<NextHealthAPIResponse<{ patients: Patient[] }>> =
       await externalApi.get(`/patients${params}`);
 
     const { data: patients } = data;
@@ -21,6 +25,30 @@ export const getPatients = async (params: string) => {
           `Failed to fetch patients: ${
             error instanceof Error ? error.message : "Unknown error"
           }`
+        );
+  }
+};
+
+export const createPatient = async (
+  params: string,
+  payload: CreatePatientBodyInput
+) => {
+  try {
+    const { data }: AxiosResponse<NextHealthAPIResponse<Patient>> =
+      await externalApi.post(`/patients${params}`, payload);
+
+    const { data: patients } = data;
+
+    return patients;
+  } catch (error) {
+    throw error instanceof ApiError
+      ? error
+      : new ApiError(
+          500,
+          `Failed to create a patient: ${
+            error instanceof Error ? error.message : "Unknown error"
+          }`,
+          false
         );
   }
 };
