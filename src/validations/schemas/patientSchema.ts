2b3b4ac0abbff1149e4dc4b538661686a07393ce
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-22 12:16:47 +0500

    Validation: Create Patient Form

--- src/validations/schemas/patientSchema.ts
+++ src/validations/schemas/patientSchema.ts
@@ -25,3 +25,31 @@ export const GetPatientsSchema = z.object({
 });
 
 export type GetPatientsInput = z.infer<typeof GetPatientsSchema>;
+
+export const CreatePatientParamsSchema = z.object({
+  subdomain: z.string().min(1, "Subdomain is required."),
+  location_id: z.string().optional(),
+});
+
+export const CreatePatientSchema = z.object({
+  provider: z.object({
+    provider_id: z.number(),
+  }),
+  patient: z.object({
+    first_name: z.string(),
+    last_name: z.string(),
+    email: z.string().email(),
+    bio: z.object({
+      gender: z.enum(["Male", "Female", "Other"]).optional(), // optional enum if needed
+      phone_number: z.string().min(7).max(15),
+      date_of_birth: z.string().regex(/^\d{4}-\d{2}-\d{2}$/, {
+        message: "Must be a valid YYYY-MM-DD date string",
+      }),
+    }),
+  }),
+});
+
+export type CreatePatientParamsInput = z.infer<
+  typeof CreatePatientParamsSchema
+>;
+export type CreatePatientBodyInput = z.infer<typeof CreatePatientSchema>;
