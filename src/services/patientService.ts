077737561e39cc47d14bbeb80fd1e8385bbbfdb3
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-22 10:42:19 +0500

    Patient Service: Get Patients

--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -3,11 +3,11 @@ import externalApi from "../utils/externalApi";
 
 import ApiError from "../utils/ApiError";
 
-import { NextHealthAPIResponse } from "../types";
+import { NextHealthAPIResponse, Patient } from "../types";
 
 export const getPatients = async (params: string) => {
   try {
-    const { data }: AxiosResponse<NextHealthAPIResponse<any[]>> =
+    const { data }: AxiosResponse<NextHealthAPIResponse<Patient[]>> =
       await externalApi.get(`/patients${params}`);
 
     const { data: patients } = data;
