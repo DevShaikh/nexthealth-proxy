495d69bc00cb3fa881acaa85f138592df2e118d7
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-22 12:15:59 +0500

    Patient Controller: Create Patient

--- src/controllers/patientController.ts
+++ src/controllers/patientController.ts
@@ -2,10 +2,16 @@ import { RequestHandler } from "express";
 
 import { sendSuccessResponse } from "../utils/responseHandler";
 
-import { GetPatientsInput } from "../validations/schemas/patientSchema";
+import {
+  CreatePatientBodyInput,
+  CreatePatientParamsInput,
+  GetPatientsInput,
+} from "../validations/schemas/patientSchema";
+
+import { toQueryParams } from "../utils";
+import ApiError from "../utils/ApiError";
 
 import * as patientService from "../services/patientService";
-import { toQueryParams } from "../utils";
 
 export const getPatients: RequestHandler = async (req, res, next) => {
   try {
@@ -15,6 +21,33 @@ export const getPatients: RequestHandler = async (req, res, next) => {
     const result = await patientService.getPatients(params);
 
     sendSuccessResponse(res, 200, result, "Patients fetched successful");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createPatient: RequestHandler = async (req, res, next) => {
+  try {
+    const query = req.query as CreatePatientParamsInput;
+    const params = toQueryParams(query);
+
+    const payload = req.body as CreatePatientBodyInput;
+
+    const get_patients_params = toQueryParams({
+      ...query,
+      name: `${payload.patient.first_name} ${payload.patient.last_name}`,
+      date_of_birth: payload.patient.bio.date_of_birth,
+    });
+
+    const { patients } = await patientService.getPatients(get_patients_params);
+
+    if (patients.length) {
+      throw new ApiError(409, "Patient already exists");
+    }
+
+    const result = await patientService.createPatient(params, payload);
+
+    sendSuccessResponse(res, 200, result, "Patients created successful");
   } catch (err) {
     next(err);
   }
