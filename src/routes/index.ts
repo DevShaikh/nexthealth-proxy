2fa14cad268ddbaedd3e503985dd904c78d753dd
Ahmed Shaikh <asofficial4k@gmail.com>
2025-07-22 09:41:52 +0500

    Auth Routes: Get Token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 
+import authRoutes from "./authRoutes";
 import locationRoutes from "./locationRoutes";
 import providerRoutes from "./providerRoutes";
+import appointmentRoutes from "./appointmentRoutes";
 
 const router = Router();
 
@@ -10,8 +12,12 @@ router.get("/", (_, res) => {
   res.json({ message: "Welcome to the Health Care Appointment API" });
 });
 
+router.use("/authenticates", authRoutes);
+
 router.use("/locations", locationRoutes);
 
 router.use("/providers", providerRoutes);
+
+router.use("/appointment", appointmentRoutes);
 
 export default router;
